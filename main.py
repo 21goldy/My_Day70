bdb322e370ce7a050c168d7abee7119fd32f3e5b
Goldy <21goldygour@gmail.com>
2021-07-06 04:25:37 +0530

    Initial commit

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def contact():
 
 
 @app.route("/new-post", methods=["GET", "POST"])
-# @admin_only
+@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -197,7 +197,7 @@ def add_new_post():
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-# @admin_only
+@admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
