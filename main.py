1af2d982eff3c82f211ed85349a3053f0d13d8ff
Goldy <21goldygour@gmail.com>
2021-07-06 02:30:36 +0530

    Initial commit

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def register():
 
         if User.query.filter_by(email=form.email.data).first():
             print(User.query.filter_by(email=form.email.data).first())
-            #User already exists
+            # User already exists
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for('login'))
 
