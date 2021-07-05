82888cdc2224906a426a9068a59156253dbf95f7
Goldy <21goldygour@gmail.com>
2021-07-06 03:16:25 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import date
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
